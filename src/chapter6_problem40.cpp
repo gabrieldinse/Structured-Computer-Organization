ee5dcac75f9c834c06a4ee7ac78b180d57bcc630
Gabriel Dinse <gabriel.dinse@outlook.com>
2026-03-07 17:52:10 -0300

    Chapter 6, problem 40: added directory iteration using Linux (UNIX) API

---
+++ src/chapter6_problem40.cpp
@@ -0,0 +1,22 @@
+
+#include "gtest/gtest.h"
+
+#include <print>
+#include <string>
+
+#include <dirent.h>
+#include <sys/types.h>
+
+namespace SCO {
+TEST(ListDir, UNIX) {
+  std::string rootDir = "../testing_dir";
+  auto dirPointer = opendir(rootDir.c_str());
+  while (auto dirEntry = readdir(dirPointer)) {
+    struct stat stat;
+    std::string filepath = rootDir + "/" + dirEntry->d_name;
+    lstat(filepath.c_str(), &stat);
+    std::print("[Dir entry] Name: '{}', Size: {} Bytes\n", dirEntry->d_name,
+               stat.st_size);
+  }
+}
+} // namespace SCO
